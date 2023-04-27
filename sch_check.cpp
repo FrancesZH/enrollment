c47f5ce1ae529ea5a5d6c13eb94800d77480e06a
Chen Ming Huang <chuang99@cs.binghamton.edu>
2023-04-27 00:58:14 -0400

    hmmm

--- sch_check.cpp
+++ sch_check.cpp
@@ -1,9 +1,25 @@
-
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+
 using namespace std;
+
+unordered_map<string, courseNode> readPrerequisites(string filename)
+{
+    ifstream infile(filename);
+    string line;
+    while (getline(infile, line))
+    {
+
+    }
+}    
 
 int main(int argc, char *argv[])
 {
-    string prereqfile;
-    string schedulefile;
+    string prereqfile = argv[1];
+    string schedulefile = argv[2];
+
+    
 }
