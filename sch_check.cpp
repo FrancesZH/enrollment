2c330019de5b481c8fe77f9b59d4c35ec997a77f
Saharshini Eppakayala <seppaka1@cs.binghamton.edu>
2023-04-27 20:18:41 -0400

    do not work for the or case

--- sch_check.cpp
+++ sch_check.cpp
@@ -10,10 +10,33 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    // Read prerequisites file
     string prereqfile = argv[1];
+    ifstream prereqs(prereqfile);
+    unordered_map<string, unordered_set<string>> prerequisites;
+
+    string line;
+    while (getline(prereqs, line))
+    {
+        if (line != "")
+        {
+            stringstream currentLine(line);
+            string course, prereq;
+            currentLine >> course;
+
+            // Create new set for course if it doesn't exist
+            if (!prerequisites.count(course))
+                prerequisites[course] = unordered_set<string>();
+
+            // Add each prerequisite to the set for this course
+            while (currentLine >> prereq)
+                prerequisites[course].insert(prereq);
+        }
+    }
+
+    // Read schedule file
     string schedulefile = argv[2];
     ifstream schedule(schedulefile);
-    string line;
     unordered_set<string> courses;
     unordered_map<string, int> coursesPerSemester;
 
@@ -27,11 +50,26 @@ int main(int argc, char *argv[])
             if (courses.count(course))
                 cout << "Duplicate course" << endl;
             else
-                courses.insert(course);
+            {
+                // Check if all prerequisites have been taken
+                bool allPrereqsTaken = true;
+                for (string prereq : prerequisites[course])
+                {
+                    if (!courses.count(prereq))
+                    {
+                        allPrereqsTaken = false;
+                        cout << course <<" "<< "Prerequisite not taken: " << prereq << endl;
+                    }
+                }
+
+                if (allPrereqsTaken)
+                    courses.insert(course);
+            }
+
             currentLine >> semester;
             coursesPerSemester[semester]++;
             if (coursesPerSemester[semester] > 3)
-                cout << "more than 3 course are taken" << endl;
+                cout << "more than 3 courses taken" << endl;
             cout << coursesPerSemester[semester] << endl;
         }
     }
