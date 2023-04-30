bd8c0840b465a92f896990a903b7959e80136265
Saharshini Eppakayala <seppaka1@cs.binghamton.edu>
2023-04-30 00:40:25 -0400

    it prints the prerequisite which is not taken and works for or case

--- sch_check.cpp
+++ sch_check.cpp
@@ -8,28 +8,33 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Read prerequisites file
     string prereqfile = argv[1];
     ifstream prereqs(prereqfile);
     unordered_map<string, vector<vector<string>>> prerequisites;
     string line;
 
-    while (getline(prereqs, line)) {
+    while (getline(prereqs, line))
+    {
         vector<string> prereqVector;
         string course, prereq;
 
-        if (line != "") {
+        if (line != "")
+        {
             stringstream currentLine(line);
             currentLine >> course;
 
             // Create new set for course if it doesn't exist
-            if (!prerequisites.count(course)) {
+            if (!prerequisites.count(course))
+            {
                 prerequisites[course] = vector<vector<string>>();
             }
 
             // Add each prerequisite to the set for this course
-            while (currentLine >> prereq) {
+            while (currentLine >> prereq)
+            {
                 prereqVector.push_back(prereq);
             }
 
@@ -44,51 +49,71 @@ int main(int argc, char *argv[]) {
     unordered_set<string> courses;
     unordered_map<string, int> coursesPerSemester;
 
-    while (getline(schedule, line)) {
-        if (line != "") {
+    while (getline(schedule, line))
+    {
+        if (line != "")
+        {
             stringstream currentLine(line);
             string course, semester;
             currentLine >> course;
 
-            if (courses.count(course)) {
+            if (courses.count(course))
+            {
                 cout << "Duplicate course" << endl;
-            } else {
+            }
+            else
+            {
                 // Check if all prerequisites have been taken
                 bool allPrereqsTaken = true;
-                for (const vector<string>& prereqVector : prerequisites[course]) {
+                for (const vector<string> &prereqVector : prerequisites[course])
+                {
                     bool allPrereqsTakenInVector = false;
-                    for (const string& prereq : prereqVector) {
-                        if (courses.count(prereq)) {
+                    for (const string &prereq : prereqVector)
+                    {
+                        if (courses.count(prereq))
+                        {
                             allPrereqsTakenInVector = true;
                             break;
                         }
                     }
 
-                    if (!allPrereqsTakenInVector) {
+                    if (!allPrereqsTakenInVector)
+                    {
                         allPrereqsTaken = false;
-                        cout << course << " Prerequisite not taken" << endl;
+                        for (const string &prereq : prereqVector)
+                        {
+                            if (!courses.count(prereq))
+                            {
+                                cout << course << " Prerequisite not taken: " << prereq << endl;
+                            }
+                        }
                         break;
                     }
                 }
 
-                if (allPrereqsTaken) {
+                if (allPrereqsTaken)
+                {
                     courses.insert(course);
                 }
             }
 
             currentLine >> semester;
             coursesPerSemester[semester]++;
-            if (coursesPerSemester[semester] > 3) {
+            if (coursesPerSemester[semester] > 3)
+            {
                 cout << "more than 3 courses taken" << endl;
             }
         }
     }
 
-    for (const auto& course : prerequisites) {
+    for (const auto &course : prerequisites)
+    {
         cout << course.first << ": ";
-        for (const auto& prerequisites_list : course.second) {
+        for (const auto &prerequisites_list : course.second)
+        {
             cout << "[";
-            for (const auto& prerequisite : prerequisites_list) {
+            for (const auto &prerequisite : prerequisites_list)
+            {
                 cout << prerequisite << " ";
             }
             cout << "]";
