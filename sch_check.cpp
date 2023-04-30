eea37011ce7245293ccaedb5d40d64b904d16bae
Saharshini Eppakayala <seppaka1@cs.binghamton.edu>
2023-04-30 00:32:07 -0400

    it works for the or case of prerequisites

--- sch_check.cpp
+++ sch_check.cpp
@@ -5,99 +5,96 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <vector>
+
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     // Read prerequisites file
     string prereqfile = argv[1];
     ifstream prereqs(prereqfile);
     unordered_map<string, vector<vector<string>>> prerequisites;
     string line;
 
-    while (getline(prereqs, line))
-    {
+    while (getline(prereqs, line)) {
         vector<string> prereqVector;
         string course, prereq;
 
-        if (line != "")
-        {
+        if (line != "") {
             stringstream currentLine(line);
             currentLine >> course;
 
             // Create new set for course if it doesn't exist
-            if (!prerequisites.count(course))
-            {
+            if (!prerequisites.count(course)) {
                 prerequisites[course] = vector<vector<string>>();
             }
 
             // Add each prerequisite to the set for this course
-            while (currentLine >> prereq)
+            while (currentLine >> prereq) {
                 prereqVector.push_back(prereq);
+            }
+
             // Assign the vector of prerequisites to the key in the unordered_map
             prerequisites[course].push_back(prereqVector);
         }
+    }
 
-        // Read schedule file
-        string schedulefile = argv[2];
-        ifstream schedule(schedulefile);
-        unordered_set<string> courses;
-        unordered_map<string, int> coursesPerSemester;
+    // Read schedule file
+    string schedulefile = argv[2];
+    ifstream schedule(schedulefile);
+    unordered_set<string> courses;
+    unordered_map<string, int> coursesPerSemester;
 
-        while (getline(schedule, line))
-        {
-            if (line != "")
-            {
-                stringstream currentLine(line);
-                string course, semester;
-                currentLine >> course;
-                if (courses.count(course))
-                    cout << "Duplicate course" << endl;
-                else
-                {
-                    // Check if all prerequisites have been taken
-                    bool allPrereqsTaken = true;
-                    for (const vector<string> &prereqVector : prerequisites[course])
-                    {
-                        bool allPrereqsTakenInVector = true;
-                        for (const string &prereq : prereqVector)
-                        {
-                            if (!courses.count(prereq))
-                            {
-                                allPrereqsTakenInVector = false;
-                                cout << course << " Prerequisite not taken: " << prereq << std::endl;
-                                break; // Exit the inner loop as soon as a prerequisite that has not been taken is found
-                            }
-                        }
-                        if (!allPrereqsTakenInVector)
-                        {
-                            allPrereqsTaken = false;
-                            break; // Exit the outer loop as soon as a vector of prerequisites that has not been taken is found
+    while (getline(schedule, line)) {
+        if (line != "") {
+            stringstream currentLine(line);
+            string course, semester;
+            currentLine >> course;
+
+            if (courses.count(course)) {
+                cout << "Duplicate course" << endl;
+            } else {
+                // Check if all prerequisites have been taken
+                bool allPrereqsTaken = true;
+                for (const vector<string>& prereqVector : prerequisites[course]) {
+                    bool allPrereqsTakenInVector = false;
+                    for (const string& prereq : prereqVector) {
+                        if (courses.count(prereq)) {
+                            allPrereqsTakenInVector = true;
+                            break;
                         }
                     }
-                    if (allPrereqsTaken)
-                        courses.insert(course);
+
+                    if (!allPrereqsTakenInVector) {
+                        allPrereqsTaken = false;
+                        cout << course << " Prerequisite not taken" << endl;
+                        break;
+                    }
                 }
-                currentLine >> semester;
-                coursesPerSemester[semester]++;
-                if (coursesPerSemester[semester] > 3)
-                    cout << "more than 3 courses taken" << endl;
-                // cout << coursesPerSemester[semester] << endl;
+
+                if (allPrereqsTaken) {
+                    courses.insert(course);
+                }
+            }
+
+            currentLine >> semester;
+            coursesPerSemester[semester]++;
+            if (coursesPerSemester[semester] > 3) {
+                cout << "more than 3 courses taken" << endl;
             }
         }
     }
-    for (const auto &course : prerequisites)
-    {
+
+    for (const auto& course : prerequisites) {
         cout << course.first << ": ";
-        for (const auto &prerequisites_list : course.second)
-        {
+        for (const auto& prerequisites_list : course.second) {
             cout << "[";
-            for (const auto &prerequisite : prerequisites_list)
-            {
+            for (const auto& prerequisite : prerequisites_list) {
                 cout << prerequisite << " ";
             }
             cout << "]";
         }
         cout << endl;
     }
+
+    return 0;
 }
